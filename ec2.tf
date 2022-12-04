2f9ab58312213089b728578e19e91a44189475fc
root <root@ip-172-31-4-20.ec2.internal>
2022-12-04 22:04:21 +0000

    Edit file ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,24 @@
 
+module "apache_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "apache-sg"
+  description = "Security group para o servidor do Apache"
+  vpc_id      = data.aws_vpc.PROJETO-MBA-VPC.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+  egress_rules        = ["all-all"]
+}
+
+
+
 resource "aws_instance" "instancia_I" {
-  ami                  = "ami-01a73f51321ab6899"
-  instance_type        = "t3.micro"
+  ami                  = "ami-0b0dcb5067f052a63"
+  instance_type        = "t2.micro"
   key_name             = "vockey"
+  monitoring             = true
+  vpc_security_group_ids = [module.apache_sg.security_group_id]
   subnet_id            = "subnet-0a6185b9c5eaa2fca"
   iam_instance_profile = "LabInstanceProfile"
   user_data            = file("./dependencias_ec2.sh")
@@ -17,8 +33,8 @@ resource "aws_instance" "instancia_I" {
 
 
 resource "aws_instance" "instancia_II" {
-  ami           = "ami-01a73f51321ab6899"
-  instance_type = "t3.micro"
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
   key_name      = "vockey"
   subnet_id     = "subnet-0cf8fc8cb765d8e6e"
 
