ebaa51148ff3cbc674c76e6fe69c8ca765f95c9b
root <root@ip-172-31-5-3.ec2.internal>
2022-12-05 01:49:28 +0000

    Edit index.html

--- dependencias_ec2-II.sh
+++ dependencias_ec2-II.sh
@@ -9,5 +9,5 @@ yum install httpd -y
 systemctl start httpd
 systemctl enabled httpd
 
-echo "<h1>INSTANCIA-II</h1>" > /var/www/html/index.html
+echo "<h1>INSTANCIA 2 do load balancers</h1>" > /var/www/html/index.html
 
--- dependencias_ec2.sh
+++ dependencias_ec2.sh
@@ -9,4 +9,4 @@ yum install httpd -y
 systemctl start httpd
 systemctl enabled httpd
 
-echo "<h1>INSTANCIA-I</h1>" > /var/www/html/index.html
+echo "<h1>INSTANCIA 1 do load balancers</h1>" > /var/www/html/index.html
